98283dfb169e113c6590ef3d234bbd6e65954434
Till Hoffmann <till.hoffmann@enforge.de>
2019-11-22 11:47:42 +0100

    adding custom role for windows

---
+++ windows/custom.ps1
@@ -0,0 +1,34 @@
+# show file extensions
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HidefileExt' -Value 0
+
+# show all icons in traybar
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer' -Name 'EnableAutoTray' -Value 0
+
+# remove search field from taskbar
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -Name 'SearchboxTaskbarMode' -Value 0
+
+# pin explorer ribbon
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Ribbon' -Name 'MinimizedStateTabletModeOff' -Value 0
+
+# set explorer to open 'This PC' by default instead of 'Quick access'
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'LaunchTo' -Value 1
+#   1 == This PC
+#   2 == Quick access
+#   3 == Downloads
+
+# set explorer to always show full path
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer' -Name 'CabinetState' -Value 1
+#   0 == display name
+#   1 == display full path
+
+# set explorer to never show recently used files in quick access
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer' -Name 'ShowRecent' -Value 0
+# set explorer to never show frequently used files in quick access
+Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer' -Name 'ShowFrequent' -Value 0
+# delete history of recently and frequently used files
+rm -recurse -force %appdata%/Microsoft/Windows/Recent/*
+# restart explorer afterwards
+taskkill /IM explorer.exe /F; explorer.exe
+
+# remove everything from desktop
+rm $HOME\Desktop\*
--- windows_10.ps1
+++ windows_10.ps1
@@ -6,8 +6,17 @@
 ## install scoop
 ./windows/install_scoop.ps1
 
+
+# variables
+$USER = "dev"
+#Get credentials for given user
+$Credential = Get-Credential $USER
+
 # roles
 ./windows/common.ps1
 ./windows/dev.ps1
 ./windows/docker.ps1
 #./windows/custom.ps1
+$customconfigjob = Start-Job -ScriptBlock {./windows/custom.ps1} -Credential $Credential
+Wait-Job $customconfigjob
+Receive-Job -Job $customconfigjob
