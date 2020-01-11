55bf90d176f92f5c5442e6e21f9b15ae41294852
till.hoffmann <till.hoffmann@enforge.de>
2020-01-11 17:06:34 +0100

    added hide.ps1 into a new script folder

---
+++ windows/scripts/hide.ps1
@@ -0,0 +1,15 @@
+# add hidden attribute to all files and folders in the current directory which names start with a dot.
+
+Get-ChildItem . | ForEach-Object {
+  if ( $_ -is [System.IO.DirectoryInfo]) { # if element is directory
+    if ( $_.BaseName.length -gt 1 -and $_.BaseName.Substring(0,1) -eq '.' ) {
+      $_.Attributes="Hidden";
+      echo "folder $($_.BaseName) is now hidden";
+    }
+  } else { # else element is file
+    if ( $_.BaseName.Length -eq 0 -and $_.Extension.Length -gt 1 ) {
+      $_.Attributes="Hidden";
+      echo "file $($_.Extension) is now hidden";
+    }
+  }
+}
