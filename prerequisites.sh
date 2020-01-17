006b2b451baf6ed5afa077c570ad843c78358233
till.hoffmann <till.hoffmann@enforge.de>
2020-01-17 21:27:17 +0100

    added upgrade & automated software installation

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install software-properties-common
+apt upgrade -y
+apt install -y software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install -y ansible
