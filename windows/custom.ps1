652eefe26475beb5f788c5bde6e4e1fe5c538b53
Till Hoffmann <till.hoffmann@enforge.de>
2020-02-07 17:07:43 +0100

    fixed locations and commands

--- windows/custom.ps1
+++ windows/custom.ps1
@@ -40,7 +40,7 @@ Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\P
 #Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\DWM' -Name 'ColorPrevalence' -Value 1
 
 # add scripts to C:/Windows, which is included in path
-cp ./scripts/hide.ps1 C:/Windows/hide.ps1
+Copy-Item ./windows/scripts/hide.ps1 C:/Windows/hide.ps1
 
 # remove everything from desktop
-rm $HOME\Desktop\*
+Remove-Item $HOME\Desktop\*
