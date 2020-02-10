d9fc0ac7129fa6416e13401828f45ea01d50bb7d
till.hoffmann <till.hoffmann@enforge.de>
2020-02-10 13:40:58 +0100

    added restartrequired variable

--- windows_10.ps1
+++ windows_10.ps1
@@ -11,6 +11,7 @@
 #$USER = $env:UserName
 #Get credentials for given user
 #$Credential = Get-Credential $USER
+$restartrequired = $false
 
 # roles
 ./windows/common.ps1
@@ -20,27 +21,27 @@
 #$customconfigjob = Start-Job -ScriptBlock {./windows/custom.ps1} -Credential $Credential
 #Wait-Job $customconfigjob
 #Receive-Job -Job $customconfigjob
+
+if ($restartrequired){
+  Write-Output "Restart required!"
+}
+
 Write-Output "What is now missing:"
 Write-Output "- install office, Visio"
 Write-Output "- install enpass"
 Write-Output "- install Microsoft-ToDo (from store)"
 Write-Output "- install whatsapp (store)"
 Write-Output "- Install veeam / configure backup"
-Write-Output "- install flux"
 Write-Output "? install advanced port scanner"
 Write-Output "- install games/launchers"
-Write-Output "? install java/ecplise"
+Write-Output "? install ecplise"
 Write-Output "- install JDownloader"
 Write-Output "- install Firefox"
 Write-Output "- install nodejs"
 Write-Output "- install netspeedmonitor"
 Write-Output "- install openshot video editor"
-Write-Output "- install paint.net"
 Write-Output "- install putty (path?/onedrive?)"
 Write-Output "- install Teamspeak"
-Write-Output "- install Teamviewer"
-Write-Output "- install vlc"
-Write-Output "- install windirstat"
 Write-Output ""
 Write-Output "What is now to do:"
 Write-Output "- cleaning of start menu tiles ('unpin group')"
