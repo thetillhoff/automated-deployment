35d597348330cc0aac38eef8b2a6a2a9517bcecb
till.hoffmann <till.hoffmann@enforge.de>
2020-02-10 13:38:03 +0100

    added setting of background image

--- windows/custom.ps1
+++ windows/custom.ps1
@@ -1,3 +1,6 @@
+# install f.lux
+choco install -y f.lux
+
 # show file extensions
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HidefileExt' -Value 0
 
@@ -39,6 +42,20 @@ Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\P
 ## set accent color on title bars
 #Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\DWM' -Name 'ColorPrevalence' -Value 1
 
+# delete background image history
+for ($i = 0; $i -le 5; $i++)
+{
+  if (Test-RegistryValue -path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" -value "BackgroundHistoryPath$i" )
+  {
+    Remove-itemproperty -path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" -name "BackgroundHistoryPath$i" 
+  }
+}
+# change background image
+cp ./windows/files/black.png C:/windows/black.png
+Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value C:/windows/black.png
+$restartrequired = $true
+
+# disable web results in start-menu-search
 Set-WindowsSearchSetting -EnableWebResultsSetting $false
 
 # disable onedrive ads
