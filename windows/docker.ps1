8cbe92001d5b02ad790ef653251029b135474cf2
Till Hoffmann <till.hoffmann@enforge.de>
2020-05-01 15:31:05 +0200

    disabled docker autostart

--- windows/docker.ps1
+++ windows/docker.ps1
@@ -1,2 +1,5 @@
 # install Docker for Windows
 choco install -y docker-desktop
+
+# disable autostart of Docker (0x03 disables, 0x02 enables, any other value seems to disable)
+Set-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run -Name "Docker Desktop" -Value ([byte[]](0x03,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00))
