5d8761f28e714ad4c6b0fe7d74e7106917a3dd4d
Till Hoffmann <till.hoffmann@enforge.de>
2020-09-07 21:18:55 +0200

    updated ansible installation

--- prerequisites.sh
+++ prerequisites.sh
@@ -3,5 +3,7 @@
 apt update
 apt upgrade -y
 apt install -y software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+apt update
 apt install -y ansible
